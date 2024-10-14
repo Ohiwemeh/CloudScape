963bd506bb08c6afb3de1d59f3ca87ddaa059cc0
Ohiwemeh <ohiwemeh@gmail.com>
2024-10-14 10:23:09 +0100

    first commit

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Page404 = () => {
   return (
     <div className='mt-60 max-w-screen-2xl container mx-auto xl:px-28 px-4'>
-      motherfucker
+   
     </div>
   )
 }
