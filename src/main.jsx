8287c9badd14d9d034aff7e2a9d65d590d0d7449
Ohiwemeh <ohiwemeh@gmail.com>
2025-02-03 17:53:02 +0100

    Hide ShopSideNav on small and tablet screens

--- src/main.jsx
+++ src/main.jsx
@@ -1,47 +1,48 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux"; // Keep the Provider
 import "./index.css";
 import App from "./App";
 import Home from "./pages/home/Home";
 import SingleProduct from "./pages/home/SingleProduct";
 import Page404 from "./pages/Page404";
-import Shopping from "./pages/home/Shopping";
-import {Provider} from "react-redux"
-import { store } from "./store";
+import Shop from  "./pages/Shop/Shop"
 
+// Mock store
+const mockStore = {
+  getState: () => ({}), // Return an empty state
+  subscribe: () => {}, // No-op function
+  dispatch: () => {}, // No-op function
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
-    children:[
+    element: <App />,
+    children: [
       {
         path: "/",
-        element: <Home/>
+        element: <Home />,
       },
       {
         path: "/shop/:id",
-        element: <SingleProduct/>
+        element: <SingleProduct />,
       },
       {
-        path:"/404",
-        element: <Page404/>
+        path: "/404",
+        element: <Page404 />,
       },
       {
-        path:"/shop",
-        element: <Shopping/>
+        path: "/shop",
+        element: <Shop/>
       }
-
-    ]
+    ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+  <Provider store={mockStore}> {/* Pass the mock store */}
     <RouterProvider router={router} />
   </Provider>
 );
